e6f0c134a3218f1b4f5b426d6f3ddebec43dabb2
Shaun Porwal <shaun.porwal@gmail.com>
2024-11-13 16:01:12 -0500

    Deploy website - based on a4edc6c934db3c0cf305f924f9ab02fda1e7ebc2

---
+++ assets/js/814f3328.d88d1afa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjuntotechnologies_github_io=self.webpackChunkjuntotechnologies_github_io||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Chronicles of a Shitty WebDev","permalink":"/blog/chronicles-of-a-shitty-webdev","unlisted":false,"date":"2024-11-07T00:00:00.000Z"}]}')}}]);
