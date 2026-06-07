15317dd618e884990f6ee71f51eacc33954f239d
Shaun Porwal <shaun.porwal@gmail.com>
2026-06-07 13:19:59 -0400

    Apply Junto palette and profile photo

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const mimeTypes = {
   '.gif': 'image/gif',
   '.html': 'text/html; charset=utf-8',
   '.ico': 'image/x-icon',
+  '.jpeg': 'image/jpeg',
   '.jpg': 'image/jpeg',
   '.js': 'text/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
